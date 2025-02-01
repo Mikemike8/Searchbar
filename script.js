531048952a5115cce89450bcbfb2cc81421d738a
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-01-31 20:31:34 -0600

    push

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const searchInput = document.querySelector("[data-search]")
 let  users = [ ]
 
 searchInput.addEventListener("input", (e) => {
+    // targeting input value
     const value = e.target.value.toLowerCase();
+   
     users.forEach(user => {
-        
-        const isVisble = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
-        user.element.classList.toggle("hide", !isVisble)
-     });
-    
+        const isVisible = user.name.toString().toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+        user.element.classList.toggle("hide", !isVisible);  // Toggle visibility based on the filter
+    });
 });
+
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -21,16 +22,26 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(data => {
 
     users = data.map(user => {
-            
+    //     <template data-user-template>
+    //     <div class="card">
+    //         <div class="header"data-header></div>
+    //         <div class="body" data-body> </div>
+    //     </div>
+    // </template>
+             // clone tempalate card
             const card = userCardTemplate.content.cloneNode(true).children[0]
+            // first div
             const header = card.querySelector("[data-header]")
+            //last div
             const body = card.querySelector("[data-body]")
+          
             header.textContent = user.name
             body.textContent = user.email
+           //adding name and email data it to the userCardContainer div to div with "[data-header]" or "[data-body]" 
             userCardContainer.append(card)
              
-
-           
+            
+        //  retuning the data only once   
             return { name: user.name , email: user.email, element: card}
             
 
